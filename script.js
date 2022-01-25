a6762ceb85f0cf15bc4d77fbad8cbaae6496664f
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-01-25 23:06:17 +0300

    Responsive website using Bootstrap

---
+++ script.js
@@ -0,0 +1,14 @@
+var Testing = document.getElementById("test");
+Testing.innerHTML = "Hello JavaScript World";
+
+function handleClick() {
+  alert("Hello everyone !");
+}
+
+var Value1 = document.getElementById("space1");
+var Value2 = document.getElementById("space2");
+
+function Sum(Value1, Value2) {
+  var sum = int(Value1) + int(Value2);
+  alert("The Sum of " + int(Value1) + " and" + int(Value2) + "is : " + sum);
+}
