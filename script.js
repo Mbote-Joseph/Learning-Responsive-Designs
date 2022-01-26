afdfd18f64f892c34dda16f9919e0cac8d9bcbcb
Mbote-Joseph <mbotejoseph001@gmail.com>
2022-01-26 23:20:16 +0300

    Javascript form

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function handleClick() {
 var Value1 = document.getElementById("space1");
 var Value2 = document.getElementById("space2");
 
-function Sum(Value1, Value2) {
-  var sum = int(Value1) + int(Value2);
-  alert("The Sum of " + int(Value1) + " and" + int(Value2) + "is : " + sum);
-}
+// function Sum(Value1, Value2) {
+//   var sum = int(Value1) + int(Value2);
+//   alert("The Sum of " + int(Value1) + " and" + int(Value2) + "is : " + sum);
+// }
